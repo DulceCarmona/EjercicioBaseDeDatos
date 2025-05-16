6b70b070a231ff98a75b30c6f11dec6b5b11388a
DulceCarmona <dulce.csj0@gmail.com>
2025-05-15 23:04:49 -0600

    Se agrega diagrama de la DB en .mwb y .png, además del script.sql para mostrar ordenes completas

---
+++ script.sql
@@ -0,0 +1,23 @@
+-- Clientes
+SELECT * FROM customers;
+
+-- Empleados
+SELECT * FROM employees;
+
+-- Oficinas
+SELECT * FROM offices;
+
+-- Detalles de pedido
+SELECT * FROM orderdetails;
+
+-- Pedidos
+SELECT * FROM orders;
+
+-- Pagos
+SELECT * FROM payments;
+
+-- Linea de Productos
+SELECT * FROM productlines;
+
+-- Productos
+SELECT * FROM products;
