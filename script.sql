245eccdbaf8237743454a09004da9087c1914eda
DulceCarmona <dulce.csj0@gmail.com>
2025-05-15 23:24:10 -0600

    Se generan JOINS para obtener las órdenes completas por cliente

--- script.sql
+++ script.sql
@@ -21,3 +21,22 @@ SELECT * FROM productlines;
 
 -- Productos
 SELECT * FROM products;
+
+
+-- Crear los JOINS necesarios para obtener las órdenes completas por cliente 
+SELECT 
+    c.customerNumber,
+    c.customerName,
+    o.orderNumber,
+    o.orderDate,
+    o.shippedDate,
+    o.status,
+    od.productCode,
+    p.productName,
+    od.quantityOrdered,
+    od.priceEach,
+    (od.quantityOrdered * od.priceEach) AS total
+FROM customers c
+JOIN orders o ON c.customerNumber = o.customerNumber
+JOIN orderdetails od ON o.orderNumber = od.orderNumber
+JOIN products p ON od.productCode = p.productCode;
